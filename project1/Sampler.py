8e29a1ba613f788f66e08d93789a7d35d46d4491
Bacor <Bas@vpn-stud-146-50-146-24.vpn.uva.nl>
2016-04-13 17:09:49 +0200

    Implemented sampler

--- project1/Sampler.py
+++ project1/Sampler.py
@@ -1,19 +1,134 @@
+from helpers import *
+from collections import Counter
+
+def Gibbygibbs(corpus, B, num_iter, P0, alpha, rho):
+    """Gibbs sampler
+
+    Args:
+        corpus: a very long string without utterance boundaries
+        B: list of boundary positions
+        num_iter: number of iterations
+        P0: a function that takes a word and returns a probability
+        alpha: the alpha hyperparam
+        rho: the rho param
+
+    Returns:
+        Returns the boundary positions
+    """
+
+    # Initialize words
+    W_before = Counter()
+    W_after = get_words_counts(corpus, B)
+
+    for t in range(num_iter):
+        print("Starting iteration %s" % t)
+
+        # Remove first word from focus
+        W_after[corpus[:B[1]]] -= 1 
+
+        for b_cur in range(1,len(corpus)+1):
+
+            # Is b_cur on the boundary?
+            cur_is_on_boundary = (b_cur in B)
+
+            # To do: make more efficient?
+            b_prev = max([b for b in B if b < b_cur])
+
+            if b_cur == len(corpus):
+                b_next = len(corpus)
+            else:
+                b_next = min([b for b in B if b > b_cur])
+
+            # Words/fragments in the focus
+            w1 = corpus[b_prev:b_next]
+            w2 = corpus[b_prev:b_cur]
+            w3 = corpus[b_cur:b_next]
+
+            # We moved to a boundary, remove w3 from W_after
+            if cur_is_on_boundary:
+                W_after[w3] -= 1
+                num_words = len(B) - 3
+            else:
+                num_words = len(B) - 2
+
+            # Counts of words in the focus
+            num_w1 = W_before[w1] + W_after[w1]
+            num_w2 = W_before[w2] + W_after[w2]
+            num_w3 = W_before[w3] + W_after[w3]    
+
+            # For debugging (ONLY ON SMALL CORPORA!)
+#             corpus2 = add_word_boundaries(corpus, sorted(B))
+#             new_B = [b+i for i,b in enumerate(B)]
+#             new_b_cur = b_cur + len([b for b in B if b < b_cur])
+#             # This prints the corpus with word boundaries,
+#             print(("%s) "%b_cur)+corpus2[:new_b_cur] + '|'+corpus2[new_b_cur:])
+#             print("\tBefore: " + " ".join(["%s(%s)" %(w,i) for w, i in W_before.items() if i>0]))    
+#             print("\tAfter: " + " ".join(["%s(%s)" %(w,i) for w, i in W_after.items() if i>0]))
+
+            # Is w_1 utterance final?
+            w1_is_final = (b_next in U)
+            w2_is_final = (b_cur in U)
+            w3_is_final = (b_next in U) # Weird: this is just w1_is_final
+
+            # num(utterance boundaries in the context)
+            num_utterances = len([u for u in U if (u <= b_prev or u > b_next) and u != 0])
+            num_u = num_utterances if w1_is_final else num_words - num_utterances
+
+            # Question: Should we use log probs for overflow ?!
+            # Probability of not inserting a boundary
+            prob_h1  = (num_w1 + alpha * P0(w1)) / (num_words + alpha)
+            prob_h1 *= (num_u + rho/2) / (num_words + rho)
+
+            # Prob of inserting a boundary
+            prob_h2  = ((num_w2 + alpha * P0(w2)) / (num_words + alpha)
+                         * (num_words - num_utterances + rho/2) / (num_words + rho))
+            prob_h2 *= ((num_w3 + int(w2 == w3) + alpha * P0(w3)) / (num_words + 1 + alpha)
+                         * (num_u + int(w2_is_final == w3_is_final) + rho/2) / (num_words + 1 + rho))
+
+            insert_boundary = (prob_h2 > prob_h1)
+
+            # Always insert boundaries at utterance boundaries ?!?!
+            if b_cur in U:
+                insert_boundary = True
+
+            # Update the contexts
+            if not cur_is_on_boundary:
+                if insert_boundary:
+                    B += [b_cur]
+                    W_after[w3] -= 1
+                    W_before[w2] += 1
+                # Else: don't do anything
+
+            else: 
+                if not insert_boundary:
+                    index = B.index(b_cur)
+                    del B[index]
+                else:
+                    W_before[w2] += 1
+
+        # Wrap up this iteration
+        W_after = W_before
+        W_before = Counter()
+    
+    return B
 
 
-import numpy as np
 
-
-# data is a list of strings - each string is one line in the data
-def initialise_random(data, boundary_param):
-
-    for line in data:
-        num_boundaries = np.random.poisson(len(line)/2)
-        boundary_indices = np.random.choice(len(line), num_boundaries)
-        print(line)
-        print(boundary_indices)
-
+    
 
 if __name__ == '__main__':
-    d = ['abcdefgh', 'valentin', 'amsterdam']
+    
+    orig_corpus = "helloworld$hereisanicestory$helloworldnostorypleaseworld"
 
-    initialise_random(d, 4)
+    # Get corpus and utterance boundaries
+    corpus, U = clean_corpus(orig_corpus)
+
+    # Our 'random' initialization of the boundaries with
+    # utterance boundaries, no duplicates and sorted
+    B = [5, 10, 12, 17, 25, 31, 36, 49]
+    B = sorted(set(B + U))
+
+    P0 = lambda w: 1
+    alpha = 1
+    rho = 2
+    print(Gibbygibbs(corpus, B, 4, P0, alpha, rho))
--- project1/helpers.py
+++ project1/helpers.py
@@ -1,6 +1,13 @@
-def clean_corpus(C, sep="$"):
+###
+# Some helper functions
+
+from collections import Counter
+
+def clean_corpus(corpus, sep="$"):
     """Removes utterance boundaries and returns clean corpus
-    with utterance boundary positions.
+    with utterance boundary positions. The i-th boundary position
+    always **preceeds** the i-th character. An initial and final
+    boundary are always included
 
     Args:
         C: a very long string separated by utterance boundaries
@@ -10,14 +17,35 @@ def clean_corpus(C, sep="$"):
         new_C: clean corpus
         U: List with utterance boundary positions
     """
-    # Make sure the corpus ends with a utterance boundary (sep)
-    if C[-1] != sep: C += sep
-        
-    # Utterance boundary positions
-    U = [i for i, w in enumerate(C) if w == sep]
-    
-    # Remove separators ($) and fix indices in U accordingly
-    new_C = C.replace(sep, "")
-    U = [U[i] - i for i in range(len(U))]
 
-    return new_C, U
+    # Utterance boundary positions
+    U = [i for i, w in enumerate(corpus) if w == sep]
+
+    # Remove separators ($) and fix indices in U accordingly
+    new_corpus = corpus.replace(sep, "")
+    U = [U[i] - i for i in range(len(U))]
+    U += [0, len(new_corpus)]
+
+    return new_corpus, sorted(list(set(U)))
+
+
+
+def add_word_boundaries(corpus, boundaries, sep="."):
+    """Adds word boundaries to a corpus by inserting a sepator at every boundary
+    """
+    out = ""
+    prev_b = 0
+    for b in boundaries:
+        out += corpus[prev_b:b] + sep
+        prev_b = b
+    return out
+
+def get_words_counts(corpus, boundaries):
+    """Counts all words in a corpus given the word boundaries"""
+    counts = Counter()
+    prev_b = boundaries[0] # always equals 0
+    for b in boundaries[1:]:
+        word = corpus[prev_b:b]
+        counts[word] += 1
+        prev_b = b
+    return counts    
