77da4914636dbad33533a9b5eb03b50de47cb932
Bacor <Bas@wcw-stud-145-109-21-135.wireless.uva.nl>
2016-04-14 14:41:00 +0200

    Optimised sampler

--- project1/Sampler_only.py
+++ project1/Sampler_only.py
@@ -1,7 +1,5 @@
 from helpers import *
 from collections import Counter
-from FileUtils import read_data
-from Initialisation import gather_uniform_phon_probs, initialise_poisson, initialise_random
 
 def Sampler(corpus, B, U, num_iter, P0, alpha, rho):
     """Gibbs sampler
@@ -108,6 +106,7 @@ def Sampler(corpus, B, U, num_iter, P0, alpha, rho):
 
 
 
+    
 
 if __name__ == '__main__':
     
@@ -119,44 +118,9 @@ if __name__ == '__main__':
     # Our 'random' initialization of the boundaries with
     # utterance boundaries, no duplicates and sorted
     B = [5, 10, 12, 17, 25, 31, 36, 49]
-
-    B = [5, 16, 26, 34]
-
     B = sorted(set(B + U))
 
     P0 = lambda w: 1
     alpha = 1
     rho = 2
     print(Sampler(corpus, B, U, 4, P0, alpha, rho))
-
-
- 
-    # data is a list of strings
-    data = read_data('data\\br-phono-test.txt')
-
-    data = ['helloworld', 'helloplanet', 'byeworld', 'byeplanet']
-
-    # computes the probabilities of all phonemes and returns a function
-    # which computes the probability of a sequence based on these probabilities
-    uni_prob = gather_uniform_phon_probs(data, 0.5)
-
-    # randomly generates word boundaries
-    B = list(initialise_poisson(data, 4))
-
-    # prepare data for function 'clean_corpus'
-    data = '$'.join((line for line in data))
-
-    corpus, U = clean_corpus(data)
-
-    B = sorted(set(B + U))
-
-    # get a shallow copy of B - just to be sure that it is not canged by the sampler
-    B_copy = B[:]
-
-    result = Sampler(corpus, B, U, 10000, uni_prob, alpha, rho)
-
-    # print the data with the initial word boundaries
-    print(''.join(['_'+corpus[i] if i in B_copy else corpus[i] for i in range(len(corpus))]))
-
-    # print the data with the inferred word boundaries
-    print(''.join(['_'+corpus[i] if i in result else corpus[i] for i in range(len(corpus))]))
